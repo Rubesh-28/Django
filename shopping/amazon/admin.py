2b532b41b53889cdb687d85067242f1387b4ef2e
Rubesh <rubeshkk282gmail.com>
2023-08-22 13:08:07 +0530

    first commit

---
+++ shopping/amazon/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+
+# Register your models here.
+from amazon.models import data1
+admin.site.register(data1)
---
+++ shopping/flipkart/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+
+from flipkart.models import data2
+admin.site.register(data2)
